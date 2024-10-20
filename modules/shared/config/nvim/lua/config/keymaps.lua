ac6de36c7203c18efd6f68e778613a4c1111c277
Bassim Shahidy <bassim101@gmail.com>
2024-10-20 01:26:48 -0400

    nvim: change split border color, remap undo, pg up/dn, plugin updates

--- modules/shared/config/nvim/lua/config/keymaps.lua
+++ modules/shared/config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 
 vim.cmd("command! Q q")
+
+-- Map Page Up and Down to behave like Ctrl-D and Ctrl-U
+vim.keymap.set("n", "<PageDown>", "<C-d>", { noremap = true, silent = true })
+vim.keymap.set("n", "<PageUp>", "<C-u>", { noremap = true, silent = true })
+
+-- Remap 'U' to redo
+vim.api.nvim_set_keymap("n", "U", ":redo<CR>", { noremap = true, silent = true })
