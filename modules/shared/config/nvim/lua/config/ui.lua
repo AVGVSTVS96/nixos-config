99a864ea8d3c6098c09231aea55c2c547155e5fb
Bassim Shahidy <bassim101@gmail.com>
2024-10-25 19:15:40 -0400

    nvim: Change `WinSeparator` to `colors.border` in `tokyonight.lua`
    Change border color with `colors.border` in `tokyonight.lua`
    Remove `ui.lua`

--- modules/shared/config/nvim/lua/config/ui.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "LazyVim/LazyVim",
-    opts = function(_, opts)
-      vim.cmd([[highlight WinSeparator guifg=#3C3F47]])
-      return opts
-    end,
-  },
-}
--- modules/shared/config/nvim/lua/plugins/tokyonight.lua
+++ modules/shared/config/nvim/lua/plugins/tokyonight.lua
@@ -6,5 +6,8 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_colors = function(colors)
+      colors.border = "#565f89"
+    end,
   },
 }
