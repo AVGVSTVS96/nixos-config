69e353defe8541b6972a72d73b9edfe3561b82cb
Bassim Shahidy <bassim101@gmail.com>
2024-10-25 02:33:47 -0400

    feat(files): Use mkOutOfStoreSymlink for lvim config

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -5,12 +5,14 @@
     "nvim" = {
       # mkOutOfStoreSymlink allows my nvim config to be symlinked directly from my nixos-config repo
       # This makes it's config files writeable, allowing nvim to pick up changes without rebuilding nix
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/shared/config/nvim";
+      source = config.lib.file.mkOutOfStoreSymlink 
+        "${config.home.homeDirectory}/nixos-config/modules/shared/config/nvim";
       recursive = true;
     };
 
     "lvim/config.lua" = {
-      source = ./config/lvim/config.lua;
+      source = config.lib.file.mkOutOfStoreSymlink 
+        "${config.home.homeDirectory}/nixos-config/modules/shared/config/lvim/config.lua";
     };
   };
 }
