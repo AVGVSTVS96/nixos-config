ebdc1b6b0fabb0f5fb6941412de2cda7c0c96e8b
Bassim Shahidy <bassim101@gmail.com>
2024-11-01 01:06:48 -0400

    fix: Strip broken nixos config, replace with basic gnome

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -2,8 +2,7 @@
 
 let 
     user = variables.user;
-    hostname = variables.hostName.nixos;
-    keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhnU4G74TmS+p" ]; 
+    hostName = variables.hostName.nixos;
 in
 {
   imports = [
@@ -25,7 +24,7 @@ in
     # Uncomment for AMD GPU
     # initrd.kernelModules = [ "amdgpu" ];
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [ "uinput" ];
+    # kernelModules = [ "uinput" ];
   };
 
   # Set your time zone.
@@ -35,7 +34,7 @@ in
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking = {
-    hostName = hostname; # Define your hostname.
+    hostName = hostName; # Define your hostName.
     useDHCP = false;
     interfaces."%INTERFACE%".useDHCP = true;
   };
@@ -78,48 +77,18 @@ in
       #   Option       "TripleBuffer" "on"
       # '';
 
-      displayManager = {
-        defaultSession = "none+bspwm";
-        lightdm = {
-          enable = true;
-          greeters.slick.enable = true;
-          background = ../../modules/nixos/config/login-wallpaper.png;
-        };
-      };
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
 
-      # Tiling window manager
-      windowManager.bspwm = {
-        enable = true;
-      };
-
-      # Turn Caps Lock into Ctrl
-      layout = "us";
-      xkbOptions = "ctrl:nocaps";
+      xkb.layout = "us";
+      # xkbOptions = "ctrl:nocaps";
 
       # Better support for general peripherals
-      libinput.enable = true;
     };
+    libinput.enable = true;
 
     # Let's be able to SSH into this machine
     openssh.enable = true;
-
-    # Sync state between machines
-    syncthing = {
-      enable = true;
-      openDefaultPorts = true;
-      dataDir = "/home/${user}/.local/share/syncthing";
-      configDir = "/home/${user}/.config/syncthing";
-      user = "${user}";
-      group = "users";
-      guiAddress = "127.0.0.1:8384";
-      overrideFolders = true;
-      overrideDevices = true;
-
-      settings = {
-        devices = {};
-        options.globalAnnounceEnabled = false; # Only sync on LAN
-      };
-    };
 
     # Enable CUPS to print documents
     # printing.enable = true;
@@ -135,86 +104,6 @@ in
     #   To find the value for a specific application, use `xprop` at the
     #   terminal and then click on a window of the application in question
     #
-    picom = {
-      enable = true;
-      settings = {
-        animations = true;
-        animation-stiffness = 300.0;
-        animation-dampening = 35.0;
-        animation-clamping = false;
-        animation-mass = 1;
-        animation-for-workspace-switch-in = "auto";
-        animation-for-workspace-switch-out = "auto";
-        animation-for-open-window = "slide-down";
-        animation-for-menu-window = "none";
-        animation-for-transient-window = "slide-down";
-        corner-radius = 12;
-        rounded-corners-exclude = [
-          "class_i = 'polybar'"
-          "class_g = 'i3lock'"
-        ];
-        round-borders = 3;
-        round-borders-exclude = [];
-        round-borders-rule = [];
-        shadow = true;
-        shadow-radius = 8;
-        shadow-opacity = 0.4;
-        shadow-offset-x = -8;
-        shadow-offset-y = -8;
-        fading = false;
-        inactive-opacity = 0.8;
-        frame-opacity = 0.7;
-        inactive-opacity-override = false;
-        active-opacity = 1.0;
-        focus-exclude = [
-        ];
-
-        opacity-rule = [
-          "100:class_g = 'i3lock'"
-          "60:class_g = 'Dunst'"
-          "100:class_g = 'Alacritty' && focused"
-          "90:class_g = 'Alacritty' && !focused"
-        ];
-
-        blur-kern = "3x3box";
-        blur = {
-          method = "kernel";
-          strength = 8;
-          background = false;
-          background-frame = false;
-          background-fixed = false;
-          kern = "3x3box";
-        };
-
-        shadow-exclude = [
-          "class_g = 'Dunst'"
-        ];
-
-        blur-background-exclude = [
-          "class_g = 'Dunst'"
-        ];
-
-        backend = "glx";
-        vsync = false;
-        mark-wmwin-focused = true;
-        mark-ovredir-focused = true;
-        detect-rounded-corners = true;
-        detect-client-opacity = false;
-        detect-transient = true;
-        detect-client-leader = true;
-        use-damage = true;
-        log-level = "info";
-
-        wintypes = {
-          normal = { fade = true; shadow = false; };
-          tooltip = { fade = true; shadow = false; opacity = 0.75; focus = true; full-shadow = false; };
-          dock = { shadow = false; };
-          dnd = { shadow = false; };
-          popup_menu = { opacity = 1.0; };
-          dropdown_menu = { opacity = 1.0; };
-        };
-      };
-    };
 
     gvfs.enable = true; # Mount, trash, and other functionalities
     tumbler.enable = true; # Thumbnail support for images
@@ -225,25 +114,22 @@ in
 
   # Video support
   hardware = {
-    opengl.enable = true;
+    graphics.enable = true;
     # pulseaudio.enable = true;
     # hardware.nvidia.modesetting.enable = true;
 
     # Enable Xbox support
     # hardware.xone.enable = true;
-
-    # Crypto wallet support
-    # ledger.enable = true;
   };
 
 
   # Add docker daemon
-  virtualisation = {
-    docker = {
-      enable = true;
-      logDriver = "json-file";
-    };
-  };
+  # virtualisation = {
+  #   docker = {
+  #     enable = true;
+  #     logDriver = "json-file";
+  #   };
+  # };
 
   # It's me, it's you, it's everyone
   users.users = {
@@ -254,12 +140,12 @@ in
         "docker"
       ];
       shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = keys;
+      # openssh.authorizedKeys.keys = keys;
     };
 
-    root = {
-      openssh.authorizedKeys.keys = keys;
-    };
+    # root = {
+    #   openssh.authorizedKeys.keys = keys;
+    # };
   };
 
   # Don't require password for users in `wheel` group for these commands
@@ -289,6 +175,7 @@ in
   environment.systemPackages = with pkgs; [
     gitAndTools.gitFull
     inetutils
+    gnome-tweaks
   ];
 
   system.stateVersion = "21.05"; # Don't change this
