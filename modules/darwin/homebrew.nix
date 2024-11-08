0fe8b1a29888429a03d81fef4d9b06cddbe09e8b
Bassim Shahidy <bassim101@gmail.com>
2024-11-08 02:21:41 -0500

    homebrew: Add option to enable brew zsh integration
    Make option to `enableZshIntegration`
    Initializes brew with `/opt/homebrew/bin/brew shellenv`

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,75 +1,97 @@
-{ variables, inputs, config, ... }:
+{ variables, inputs, config, lib, ... }:
 let
   inherit (variables) userName;
   inherit (inputs) homebrew-bundle homebrew-core homebrew-cask;
   inherit (lib) mkIf mkEnableOption;
+  cfg = config.nix-homebrew;
 in
 {
-  # TODO: Add homebrew shell integration
-  # https://github.com/zhaofengli/nix-homebrew/pull/39
-
-  # nix-homebrew homebrew module
-  # Manages homebrew installation
-  nix-homebrew = {
-    user = userName;
-    enable = true;
-    taps = {
-      "homebrew/homebrew-core" = homebrew-core;
-      "homebrew/homebrew-cask" = homebrew-cask;
-      "homebrew/homebrew-bundle" = homebrew-bundle;
+  options = {
+    # Homebrew shell integration, may be merged into nix-homebrew
+    # https://github.com/zhaofengli/nix-homebrew/pull/39
+    #
+    # TODO: PR merged, update and test using nix-darwin interactiveShellInit
+    # instead of home-manager shell config like in PR
+    #
+    nix-homebrew = {
+      enableZshIntegration = mkEnableOption "homebrew zsh integration" // {
+        default = false;
+      };
     };
-    mutableTaps = false;
-    autoMigrate = true;
   };
 
-  # nix-darwin homebrew module
-  # Manages homebrew packages
-  homebrew = {
-    enable = true;
-    onActivation.cleanup = "uninstall";
+  config = {
+    # nix-homebrew homebrew module
+    # Manages homebrew installation
+    nix-homebrew = {
+      user = userName;
+      enable = true;
+      enableZshIntegration = true;
+      taps = {
+        "homebrew/homebrew-core" = homebrew-core;
+        "homebrew/homebrew-cask" = homebrew-cask;
+        "homebrew/homebrew-bundle" = homebrew-bundle;
+      };
+      mutableTaps = false;
+      autoMigrate = true;
+    };
 
-    # Fix nix-darwin trying to untap nix-homebrew taps when uninstall is set
-    # https://github.com/zhaofengli/nix-homebrew/issues/5
-    taps = builtins.attrNames config.nix-homebrew.taps;
+    # Homebrew shell integration
+    home-manager.users.${userName}.programs = {
+      zsh.initExtra = mkIf cfg.enableZshIntegration ''
+        eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
+      '';
+    };
 
-    casks = [
-      # Development Tools
-      # "homebrew/cask/docker"
-      # "visual-studio-code"
-      "visual-studio-code@insiders"
+    # nix-darwin homebrew module
+    # Manages homebrew packages
+    homebrew = {
+      enable = true;
+      onActivation.cleanup = "uninstall";
 
-      # Communication Tools
-      # "discord"
-      # "notion"
-      # "slack"
-      # "telegram"
-      # "zoom"
+      # Fixes nix-darwin trying to untap nix-homebrew taps when uninstall is set
+      # https://github.com/zhaofengli/nix-homebrew/issues/5
+      taps = builtins.attrNames config.nix-homebrew.taps;
 
-      # Utility Tools
-      # "syncthing"
+      casks = [
+        # Development Tools
+        # "homebrew/cask/docker"
+        # "visual-studio-code"
+        "visual-studio-code@insiders"
 
-      # Entertainment Tools
-      # "iina" # Video player
+        # Communication Tools
+        # "discord"
+        # "notion"
+        # "slack"
+        # "telegram"
+        # "zoom"
 
-      # Productivity Tools
-      "raycast"
+        # Utility Tools
+        # "syncthing"
 
-      # Browsers
-      "google-chrome"
-      # "arc" # Arc browser
-    ];
+        # Entertainment Tools
+        # "iina" # Video player
 
-    # Mac App Store Apps
-    # These app IDs are from using the mas CLI app
-    # $ nix shell nixpkgs#mas
-    # $ mas search <app name>
-    #
-    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
-    # you may receive an error message "Redownload Unavailable with This Apple ID".
-    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
-    # masApps = {
-    #   "1password" = 1333542190;
-    #   "wireguard" = 1451685025;
-    # };
+        # Productivity Tools
+        "raycast"
+
+        # Browsers
+        "google-chrome"
+        # "arc" # Arc browser
+      ];
+
+      # Mac App Store Apps
+      # These app IDs are from using the mas CLI app
+      # $ nix shell nixpkgs#mas
+      # $ mas search <app name>
+      #
+      # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
+      # you may receive an error message "Redownload Unavailable with This Apple ID".
+      # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
+      # masApps = {
+      #   "1password" = 1333542190;
+      #   "wireguard" = 1451685025;
+      # };
+    };
   };
 }
