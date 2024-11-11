30a81571b4a264abd860693f8cbc035391692ed0
Bassim Shahidy <bassim101@gmail.com>
2024-11-10 23:08:27 -0500

    feat: Add fish shell option, put optional shells `in shared/shells/``
    - Move zsh config to `modules/shared/shells/zsh.nix`, add fish config to
    `modules/shared/shells/fish.nix`
    - Make each shell optional, add activeShell option definition in
    `shared/default.nix` for tracking the currently enabled shell. This is
    set in each shells/ file, then used in programs.nix to enable shell
    specific integrations with `isZsh` or `isFish`

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,23 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
     ./nix.nix
+    ./shells/zsh.nix
+    ./shells/fish.nix
   ];
+
+  options.shells = {
+    # Provides `shells.activeShell` option used in ./shells/*
+    # Each shell module sets `shells.activeShell` to the shell name
+    #
+    # Programs can enable shell specific options with `shells.activeShell`:
+    # `enableFishIntegration = config.shells.activeShell == "fish";`
+    #
+    activeShell = lib.mkOption {
+      type = lib.types.enum [ "none" "zsh" "fish" "bash" ];
+      default = "none";
+      description = "Currently active shell";
+    };
+  };
 }
