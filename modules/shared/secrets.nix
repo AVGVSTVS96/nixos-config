bc5fe6a628163972308a376cfc839d46033fb158
Bassim Shahidy <bassim101@gmail.com>
2024-11-16 02:10:04 -0500

    Use dynamic nix reference for user home path
    Instead of harcoding the different home paths for darwin and linux, use
    config.users.users.${userName}.home to refer to the home directory on
    both linux and darwin dynamically

--- modules/shared/secrets.nix
+++ modules/shared/secrets.nix
@@ -1,42 +1,51 @@
-{ variables, inputs, pkgs, ... }:
+{ variables, inputs, pkgs, config, ... }:
 let
   inherit (variables) userName;
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
+  homeDir = config.users.users.${userName}.home;
 in
-{
-  # This file configures and references secrets in the `secrets` directory
-  #
-  # The primary.age file is made available to nixos here
-  # This can be referenced in the nixos config with /run/agenix/primary
-  # TODO: See if config.age.secretsDir/primary also works
-  age.secrets = {
-    primary = {
-      file = ../../secrets/primary.age;
+  {
+  # Configure agenix secrets for nix config. Access via:
+  # `config.age.secrets.<name>.path` or `/run/agenix/<name>`
 
-      # Makes file accessible by user
-      owner = userName;
-      mode = "600";
-    };
+  age = {
+    # This is the master key - it is used to decrypt all other keys
+    # This key is copied to the `.secrets` directory on the target system
+    identityPaths = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "${homeDir}/.secrets/master.age.key"
+    ];
+    
+    secrets = {
+      primary = {
+        file = ../../secrets/primary.age;
+        owner = userName;
+        mode = "600";
+      };
 
-    graphite = {
-      file = ../../secrets/graphite.age;
+      graphite = {
+        file = ../../secrets/graphite.age;
 
-      # Path to place decrypted file
-      # This is necessary because there is no way to 
-      # configure graphite in nixos, so we encrpyt and 
-      # place the full file where graphite expects it
-      path = if isDarwin 
-        then "/Users/${variables.userName}/.config/graphite/user_config"
-        else "/home/${variables.userName}/.config/graphite/user_config";
-      owner = userName;
-      mode = "600";
+        # Path to place decrypted file
+        # This is necessary because there is no way to
+        # configure graphite in nixos, so we decrpyt and
+        # place the full file where graphite expects it
+        path = if isDarwin
+          then "${homeDir}/.config/graphite/user_config"
+          else "${homeDir}/.config/graphite/user_config";
+        owner = userName;
+        mode = "600";
+      };
     };
   };
 
-  # Import the ragenix module in home-manager in addition to nixos configurations
+
+  # Ensure ragenix is available in home-manager
   home-manager.users.${variables.userName} = {
     imports = [ inputs.ragenix.homeManagerModules.age ];
   };
 
-  # Add ragenix cli to global ssystem packages
-  environment.systemPackages = with pkgs; [ inputs.ragenix.packages.${system}.ragenix ];
+  # Add ragenix cli to global system packages
+  environment.systemPackages = with pkgs; [
+    inputs.ragenix.packages.${system}.ragenix
+  ];
