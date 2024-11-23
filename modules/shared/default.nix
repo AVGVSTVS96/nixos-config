083624a843d225c382015983dcc5b06c6c0e6911
Bassim Shahidy <bassim101@gmail.com>
2024-11-23 13:53:45 -0500

    refactor: Separate options into options.nix
    Move options from default.nix to options.nix, import in default.nix

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,39 +1,24 @@
-{ lib, config, variables, ... }:
+{ config, variables, ... }:
 
 {
   imports = [
     ./nix.nix
+    ./options.nix
     ./secrets.nix
     ./shells/zsh.nix
     ./shells/fish.nix
   ];
 
-  options.shells = {
-    # Provides `shells.activeShell` option used in ./shells/*
-    # Each shell module sets `shells.activeShell` to the shell name
-    #
-    # Programs can enable shell specific options with `shells.activeShell`:
-    # `enableFishIntegration = config.shells.activeShell == "fish";`
-    #
-    activeShell = lib.mkOption {
-      type = lib.types.enum [ "none" "zsh" "fish" "bash" ];
-      default = "none";
-      description = "Currently active shell";
+  # TODO: look for way to add env vars as a file in nix
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
+  home-manager.users.${variables.userName} = {
+    home.sessionVariables = {
+      ANTHROPIC_API_KEY = "`cat ${config.age.secrets.anthropic.path}`";
+      OPENAI_API_KEY = "`cat ${config.age.secrets.openai.path}`";
     };
   };
 
-  config = {
-    # TODO: Find a better place to put this, maybe a new `common.nix` module 
-    # or keep it here and move the above options to a new `options.nix` module
-    environment.variables = {
-      EDITOR = "nvim";
-    };
-
-    home-manager.users.${variables.userName} = {
-      home.sessionVariables = {
-        ANTHROPIC_API_KEY = "`cat ${config.age.secrets.anthropic.path}`";
-        OPENAI_API_KEY = "`cat ${config.age.secrets.openai.path}`";
-      };
-    };
-  };
 }
