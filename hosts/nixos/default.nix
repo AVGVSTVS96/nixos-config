60d3b1c5438769ebd6c4b0cbb3d829a6541b28d1
Bassim Shahidy <122117267+AVGVSTVS96@users.noreply.github.com>
2025-04-21 17:57:03 -0400

    refactor(modularize): Add `hosts/common.nix` for common nixos settings (#12)
    ### TL;DR
    
    Refactored NixOS configuration by extracting common settings into a shared file.
    
    ### What changed?
    
    - Created a new `nixos-common.nix` file that contains shared configuration for all NixOS systems
    - Moved common settings from individual host configurations to this shared file:
      - User configuration
      - Boot loader settings
      - Time zone
      - Hardware configuration
      - Sudo rules
      - Font packages
      - Common system packages
    - Updated both `nixos/default.nix` and `vm/default.nix` to import the common configuration
    - Removed duplicated code from host-specific files
    - Added comments for clarity on future improvements to networking configuration

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -1,61 +1,20 @@
-{ pkgs, variables, ... }:
+{ variables, ... }:
 
 let
-  inherit (variables) userName;
   hostName = variables.hostName.nixos;
 in
 {
   imports = [
-    ../../modules/nixos/home-manager.nix
-    ../../modules/nixos/disk-config.nix
-    ../../modules/shared/cachix
-    ../../modules/shared
+    ../nixos-common.nix
   ];
 
   shells.zsh.enable = true;
   shells.fish.enable = false;
 
-  users.users.${userName} = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel" # Enable ‘sudo’ for the user.
-      "docker"
-    ];
-    # openssh.authorizedKeys.keys = keys;
-  };
-
-  # users.users.root = {
-  #   openssh.authorizedKeys.keys = keys;
-  # };
-
-  time.timeZone = "America/New_York";
-
-  # Use the systemd-boot EFI boot loader.
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 42;
-      };
-      efi.canTouchEfiVariables = true;
-    };
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "nvme"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-    ];
-    # Uncomment for AMD GPU
-    # initrd.kernelModules = [ "amdgpu" ];
-    kernelPackages = pkgs.linuxPackages_latest;
-    # kernelModules = [ "uinput" ];
-  };
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
+  # TODO: This should be in a system specific file
   networking = {
     inherit hostName;
     useDHCP = false;
@@ -110,51 +69,11 @@ in
   # Enable sound
   # sound.enable = true;
 
-  # Video support
-  hardware = {
-    graphics.enable = true;
-    # pulseaudio.enable = true;
-    # hardware.nvidia.modesetting.enable = true;
+  # hardware.nvidia.modesetting.enable = true;
 
-    # Enable Xbox support
-    # hardware.xone.enable = true;
-  };
+  # Enable Xbox support
+  # hardware.xone.enable = true;
 
-  # Add docker daemon
-  # virtualisation = {
-  #   docker = {
-  #     enable = true;
-  #     logDriver = "json-file";
-  #   };
-  # };
-
-  # Don't require password for users in `wheel` group for these commands
-  security.sudo = {
-    enable = true;
-    extraRules = [{
-      commands = [{
-          command = "${pkgs.systemd}/bin/reboot";
-          options = [ "NOPASSWD" ];
-        }];
-      groups = [ "wheel" ];
-    }];
-  };
-
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    emacs-all-the-icons-fonts
-    feather-font # from overlay
-    jetbrains-mono
-    font-awesome
-    noto-fonts
-    noto-fonts-emoji
-  ];
-
-  environment.systemPackages = with pkgs; [
-    gitAndTools.gitFull
-    inetutils
-    gnome-tweaks
-  ];
 
   system.stateVersion = "21.05"; # Don't change this
 }
--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -1,61 +1,20 @@
 { pkgs, variables, ... }:
 
 let
-  inherit (variables) userName;
   hostName = variables.hostName.vm;
 in
 {
   imports = [
-    ../../modules/nixos/home-manager.nix
-    ../../modules/nixos/disk-config.nix
-    ../../modules/shared/cachix
-    ../../modules/shared
+    ../nixos-common.nix
   ];
 
   shells.zsh.enable = true;
   shells.fish.enable = false;
 
-  users.users.${userName} = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel" # Enable ‘sudo’ for the user.
-      "docker"
-    ];
-    # openssh.authorizedKeys.keys = keys;
-  };
-
-  # users.users.root = {
-  #   openssh.authorizedKeys.keys = keys;
-  # };
-
-  time.timeZone = "America/New_York";
-
-  # Use the systemd-boot EFI boot loader.
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 42;
-      };
-      efi.canTouchEfiVariables = true;
-    };
-    kernelPackages = pkgs.linuxPackages_latest;
-    initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "nvme"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-    ];
-    # Uncomment for AMD GPU
-    # initrd.kernelModules = [ "amdgpu" ];
-    # kernelModules = [ "uinput" ];
-  };
-
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
+  # TODO: This should be in a system specific file
   networking = {
     inherit hostName;
     useDHCP = false;
@@ -99,48 +58,10 @@ in
   # Sound working in utm vm without this
   # sound.enable = true;
 
-  hardware.graphics.enable = true;
-  # hardware.pulseaudio.enable = true;
   # hardware.nvidia.modesetting.enable = true;
 
-  # Enable Xbox support
-  # hardware.xone.enable = true;
-  
-
-  # Add docker daemon
-  # virtualisation = {
-  #   docker = {
-  #     enable = true;
-  #     logDriver = "json-file";
-  #   };
-  # };
-  # Don't require password for users in `wheel` group for these commands
-  security.sudo = {
-    enable = true;
-    extraRules = [{
-        commands = [{
-            command = "${pkgs.systemd}/bin/reboot";
-            options = [ "NOPASSWD" ];
-          }];
-        groups = [ "wheel" ];
-      }];
-  };
-
-  fonts.packages = with pkgs; [
-    dejavu_fonts
-    emacs-all-the-icons-fonts
-    feather-font # from overlay
-    jetbrains-mono
-    font-awesome
-    noto-fonts
-    noto-fonts-emoji
-  ];
-
   environment.systemPackages = with pkgs; [
-    gitAndTools.gitFull
-    inetutils
     spice-vdagent
-    gnome-tweaks
   ];
 
   system.stateVersion = "21.05"; # Don't change this
